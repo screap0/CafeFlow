3398604ab1efa3426d6deb8c3a40736e14230236
mehmetirlgh <mehmet.irl@hotmail.com>
2025-05-04 23:46:33 +0300

    Update Menu.Designer.cs

--- CafeFlow/CafeFlow/Menu.Designer.cs
+++ CafeFlow/CafeFlow/Menu.Designer.cs
@@ -68,11 +68,12 @@
             this.flowLayoutPanel1.AutoScroll = true;
             this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(60)))), ((int)(((byte)(100)))));
             this.flowLayoutPanel1.Controls.Add(this.menuItem);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(525, 824);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(525, 835);
             this.flowLayoutPanel1.TabIndex = 0;
             this.flowLayoutPanel1.WrapContents = false;
             // 
